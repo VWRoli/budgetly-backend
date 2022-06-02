0aa998e3f9d956c23065d252def0e7d3b2034b74
VWRoli <fustroli@gmail.com>
2022-06-02 19:45:55 +0200

    feat: user route

--- index.ts
+++ index.ts
@@ -2,11 +2,17 @@ import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req: Request, res: Response) => res.send('Hello from home page'));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
