ff08ebbae4c1f4f4dfc5c8ce54f4f25f9a69cabf
VWRoli <fustroli@gmail.com>
2022-06-03 18:05:11 +0200

    feat: connect db

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import connectDB from './config/db.js';
 
 import usersRoutes from './routes/users.js';
 
 dotenv.config();
+
+connectDB();
 
 const app: Express = express();
 const PORT = process.env.PORT;
