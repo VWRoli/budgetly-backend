8a7140cdfb76e34a2436a092d03e3da4f287c9f1
VWRoli <fustroli@gmail.com>
2022-06-11 10:12:06 +0200

    chore: remove morgan

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import morgan from 'morgan';
+
 import connectDB from './config/db.js';
 
 import usersRoutes from './routes/users.js';
@@ -12,9 +12,6 @@ connectDB();
 
 const app: Express = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
