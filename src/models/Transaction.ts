38ba0aee342843c5a2c06ea321725e701a12bb6f
VWRoli <fustroli@gmail.com>
2022-07-09 09:08:54 +0200

    feat: transaction model

---
+++ src/models/Transaction.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema({
+  payee: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  categoryTitle: {
+    type: String,
+    // required: true,
+  },
+  budgetItemTitle: {
+    type: String,
+    //required: true,
+  },
+});
+
+export default mongoose.model('Transaction', TransactionSchema);
