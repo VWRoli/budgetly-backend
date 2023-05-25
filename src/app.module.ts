5f4c32bb9c291572d120ac726411137482592b42
VWRoli <fustroli@gmail.com>
2023-05-25 18:38:22 +0200

    feat: set up first endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URL,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      useNewUrlParser: true,
+      logging: true,
+      // Only enable this option if your application is in development,
+      // otherwise use TypeORM migrations to sync entity schemas:
+      // https://typeorm.io/#/migrations
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
