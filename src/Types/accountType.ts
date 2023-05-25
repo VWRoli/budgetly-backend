e75843ac18a0bcb8b4a488e018832951bed2b08e
VWRoli <fustroli@gmail.com>
2023-05-25 16:39:36 +0200

    chore: nest setup

--- src/Types/accountType.ts
+++
@@ -1,9 +0,0 @@
-export interface accountType {
-  balance: number;
-  _id?: string;
-  user_id: string;
-  name: string;
-  budgetId: string;
-  updatedAt?: string; //isostring
-  createdAt?: string; //isostring
-}
--- src/Types/userInfoReq.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { userType } from './userType';
-export interface userInfoReq extends Request {
-  user_id?: string;
-  user?: userType;
-}
