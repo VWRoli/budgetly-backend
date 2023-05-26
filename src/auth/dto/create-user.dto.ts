be00a036f0cd8a6afa31eb1d6b8c7d909f0c11bf
VWRoli <fustroli@gmail.com>
2023-05-26 08:30:51 +0200

    feat: add password and email validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,14 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({ example: 'email@budgetly.com', required: true })
+  @Transform(({ value }) => value.trim())
   readonly email: string;
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  @ApiProperty({ example: 'StrongPassword1', required: true })
   readonly password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { json } from 'express';
 import { initializeSwagger } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //secure app by setting HTTP response headers
   app.use(helmet());
+
+  //validation pipeline
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.use(json({ limit: '100kb' }));
   initializeSwagger(app);
