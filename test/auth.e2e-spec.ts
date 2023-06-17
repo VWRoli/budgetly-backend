771543f05dabce2df91467abf1f1b5ae0e711bdb
VWRoli <fustroli@gmail.com>
2023-06-17 11:15:19 +0200

    refactor: update tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -21,127 +21,133 @@ describe('Auth and user endpoints (E2E)', () => {
     await app.close();
   });
 
-  it('/auth/signup (POST) - Mismatched Passwords', async () => {
-    const signupData = {
-      email: 'test@example.com',
-      password: 'Password123',
-      confirmPassword: 'DifferentPassword', // Provide a different password here
-    };
+  describe('/auth/signup', () => {
+    it('Mismatched Passwords', async () => {
+      const signupData = {
+        email: 'test@example.com',
+        password: 'Password123',
+        confirmPassword: 'DifferentPassword', // Provide a different password here
+      };
 
-    await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(HttpStatus.BAD_REQUEST);
+      await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('Invalid Email Address', async () => {
+      const signupData = {
+        email: 'invalid-email-address', // Provide an invalid email address here
+        password: 'Password123',
+        confirmPassword: 'Password123',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('Weak Password', async () => {
+      const signupData = {
+        email: 'test@example.com',
+        password: 'weak', // Provide a weak password here
+        confirmPassword: 'weak',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('Sign up', async () => {
+      const signupData = {
+        email: 'test@example.com',
+        password: 'Password123',
+        confirmPassword: 'Password123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .expect(HttpStatus.CREATED);
+
+      expect(response.body).toBeDefined();
+      expect(response.body.access_token).toBeDefined();
+    });
+
+    it('Duplicate Signup', async () => {
+      const signupData = {
+        email: 'test@example.com',
+        password: 'Password123',
+        confirmPassword: 'Password123',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .expect(HttpStatus.FORBIDDEN);
+    });
   });
 
-  it('/auth/signup (POST) - Invalid Email Address', async () => {
-    const signupData = {
-      email: 'invalid-email-address', // Provide an invalid email address here
-      password: 'Password123',
-      confirmPassword: 'Password123',
-    };
+  describe('/auth/signup', () => {
+    it('Sign in', async () => {
+      const signinData = {
+        email: 'test@example.com',
+        password: 'Password123',
+      };
 
-    await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .expect(HttpStatus.BAD_REQUEST);
+      const response = await request(app.getHttpServer())
+        .post('/auth/signin')
+        .send(signinData)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toBeDefined();
+      expect(response.body.access_token).toBeDefined();
+      token = response.body.access_token;
+    });
+
+    it('Wrong Password', async () => {
+      const signinData = {
+        email: 'test@example.com',
+        password: 'WrongPassword', // Provide an incorrect password
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/signin')
+        .send(signinData)
+        .expect(HttpStatus.FORBIDDEN);
+    });
+
+    it('Wrong Email Address', async () => {
+      const signinData = {
+        email: 'wrong@example.com', // Provide an incorrect email address
+        password: 'Password123',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/signin')
+        .send(signinData)
+        .expect(HttpStatus.FORBIDDEN);
+    });
   });
 
-  it('/auth/signup (POST) - Weak Password', async () => {
-    const signupData = {
-      email: 'test@example.com',
-      password: 'weak', // Provide a weak password here
-      confirmPassword: 'weak',
-    };
+  describe('/auth/signup', () => {
+    it('(GET) - Get own user', async () => {
+      await request(app.getHttpServer())
+        .get('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.OK);
+    });
 
-    await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .expect(HttpStatus.BAD_REQUEST);
-  });
-
-  it('/auth/signup (POST)', async () => {
-    const signupData = {
-      email: 'test@example.com',
-      password: 'Password123',
-      confirmPassword: 'Password123',
-    };
-
-    const response = await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .expect(HttpStatus.CREATED);
-
-    expect(response.body).toBeDefined();
-    expect(response.body.access_token).toBeDefined();
-  });
-
-  it('/auth/signup (POST) - Duplicate Signup', async () => {
-    const signupData = {
-      email: 'test@example.com',
-      password: 'Password123',
-      confirmPassword: 'Password123',
-    };
-
-    await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .expect(HttpStatus.FORBIDDEN);
-  });
-
-  it('/auth/signin (POST) - ', async () => {
-    const signinData = {
-      email: 'test@example.com',
-      password: 'Password123',
-    };
-
-    const response = await request(app.getHttpServer())
-      .post('/auth/signin')
-      .send(signinData)
-      .expect(HttpStatus.OK);
-
-    expect(response.body).toBeDefined();
-    expect(response.body.access_token).toBeDefined();
-    token = response.body.access_token;
-  });
-
-  it('/auth/signin (POST) - Wrong Password', async () => {
-    const signinData = {
-      email: 'test@example.com',
-      password: 'WrongPassword', // Provide an incorrect password
-    };
-
-    await request(app.getHttpServer())
-      .post('/auth/signin')
-      .send(signinData)
-      .expect(HttpStatus.FORBIDDEN);
-  });
-
-  it('/auth/signin (POST) - Wrong Email Address', async () => {
-    const signinData = {
-      email: 'wrong@example.com', // Provide an incorrect email address
-      password: 'Password123',
-    };
-
-    await request(app.getHttpServer())
-      .post('/auth/signin')
-      .send(signinData)
-      .expect(HttpStatus.FORBIDDEN);
-  });
-
-  it('/users/me (GET) - Get own user', async () => {
-    await request(app.getHttpServer())
-      .get('/users/me')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.OK);
-  });
-
-  it('/users/me (DELETE) - Delete own user', async () => {
-    await request(app.getHttpServer())
-      .delete('/users/me')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.NO_CONTENT);
+    it('(DELETE) - Delete own user', async () => {
+      await request(app.getHttpServer())
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.NO_CONTENT);
+    });
   });
 });
--- test/budget.e2e-spec.ts
+++ test/budget.e2e-spec.ts
@@ -3,7 +3,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { CreateBudgetDto } from 'src/modules/budget/dto';
 import { UpdateBudgetDto } from 'src/modules/budget/dto/update-budget.dto';
-import { Budget } from 'src/modules/budget/entities';
 import { ECurrency } from 'src/modules/budget/enum';
 import * as request from 'supertest';
 
@@ -27,111 +26,158 @@ describe('Budget endpoints (E2E)', () => {
     await app.close();
   });
 
-  it('/auth/signup (POST)', async () => {
-    const signupData = {
-      email: 'test.budget@example.com',
-      password: 'Password123',
-      confirmPassword: 'Password123',
-    };
+  describe('Create user, save id and token', () => {
+    it('/auth/signup (POST)', async () => {
+      const signupData = {
+        email: 'test.budget@example.com',
+        password: 'Password123',
+        confirmPassword: 'Password123',
+      };
 
-    const response = await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(signupData)
-      .expect(HttpStatus.CREATED);
+      const response = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(signupData)
+        .expect(HttpStatus.CREATED);
 
-    expect(response.body).toBeDefined();
-    expect(response.body.access_token).toBeDefined();
-    token = response.body.access_token;
-  });
+      expect(response.body).toBeDefined();
+      expect(response.body.access_token).toBeDefined();
+      token = response.body.access_token;
+    });
 
-  it('/users/me (GET) - Get own user', async () => {
-    const response = await request(app.getHttpServer())
-      .get('/users/me')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.OK);
+    it('/users/me (GET) - Get own user', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.OK);
 
-    userId = response.body.id;
-  });
-
-  it('POST /budgets should create a new budget', async () => {
-    const createBudgetDto: CreateBudgetDto = {
-      name: 'My Budget',
-      currency: ECurrency.USD,
-      userId: userId,
-    };
-
-    const response = await request(app.getHttpServer())
-      .post('/budgets')
-      .set('Authorization', `Bearer ${token}`)
-      .send(createBudgetDto)
-      .expect(HttpStatus.CREATED);
-
-    const budget = response.body;
-
-    expect(budget.name).toBeDefined();
-    expect(budget.currency).toBeDefined();
-
-    budgetId = budget.id;
-  });
-
-  it('POST /budgets should throw error because budget with same currency alrady exists', async () => {
-    const createBudgetDto: CreateBudgetDto = {
-      name: 'My Budget',
-      currency: ECurrency.USD,
-      userId: userId,
-    };
-
-    await request(app.getHttpServer())
-      .post('/budgets')
-      .set('Authorization', `Bearer ${token}`)
-      .send(createBudgetDto)
-      .expect(HttpStatus.CONFLICT);
-  });
-
-  it('GET /budgets/:userId should return an array of budgets', async () => {
-    const response = await request(app.getHttpServer())
-      .get(`/budgets/${userId}`)
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.OK);
-
-    const budgets = response.body;
-
-    expect(Array.isArray(budgets)).toBe(true);
-    budgets.forEach((budget) => {
-      expect(budget.name).toBeDefined();
-      expect(budget.currency).toBeDefined();
+      userId = response.body.id;
     });
   });
 
-  it('PUT /budgets/:budgetId should update an existing budget', async () => {
-    const updateBudgetDto: UpdateBudgetDto = {
-      name: 'Updated Budget',
-      currency: ECurrency.EUR,
-    };
+  describe('POST /budgets', () => {
+    it('should create a new budget', async () => {
+      const createBudgetDto: CreateBudgetDto = {
+        name: 'My Budget',
+        currency: ECurrency.USD,
+        userId: userId,
+      };
 
-    const response = await request(app.getHttpServer())
-      .put(`/budgets/${budgetId}`)
-      .set('Authorization', `Bearer ${token}`)
-      .send(updateBudgetDto)
-      .expect(HttpStatus.OK);
+      const response = await request(app.getHttpServer())
+        .post('/budgets')
+        .set('Authorization', `Bearer ${token}`)
+        .send(createBudgetDto)
+        .expect(HttpStatus.CREATED);
 
-    const budget = response.body;
+      const budget = response.body;
 
-    expect(budget.name).toBe(updateBudgetDto.name);
-    expect(budget.currency).toBe(updateBudgetDto.currency);
+      expect(budget.name).toBeDefined();
+      expect(budget.currency).toBeDefined();
+
+      budgetId = budget.id;
+    });
+
+    it('should throw error because budget with same currency alrady exists', async () => {
+      const createBudgetDto: CreateBudgetDto = {
+        name: 'My Budget',
+        currency: ECurrency.USD,
+        userId: userId,
+      };
+
+      await request(app.getHttpServer())
+        .post('/budgets')
+        .set('Authorization', `Bearer ${token}`)
+        .send(createBudgetDto)
+        .expect(HttpStatus.CONFLICT);
+    });
+
+    it('should throw error because of invalid currency value', async () => {
+      const createBudgetDto = {
+        name: 'Update Budget again',
+        currency: 'invalid currency',
+        userId: userId,
+      };
+
+      await request(app.getHttpServer())
+        .post(`/budgets`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(createBudgetDto)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
   });
 
-  it('DELETE /budgets/:budgetId', async () => {
-    await request(app.getHttpServer())
-      .delete(`/budgets/${budgetId}`)
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.NO_CONTENT);
+  describe('GET /budgets/:userId', () => {
+    it('should return an array of budgets', async () => {
+      const response = await request(app.getHttpServer())
+        .get(`/budgets/${userId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.OK);
+
+      const budgets = response.body;
+
+      expect(Array.isArray(budgets)).toBe(true);
+      budgets.forEach((budget) => {
+        expect(budget.name).toBeDefined();
+        expect(budget.currency).toBeDefined();
+      });
+    });
   });
 
-  it('/users/me (DELETE) - Delete own user', async () => {
-    await request(app.getHttpServer())
-      .delete('/users/me')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.NO_CONTENT);
+  describe('PUT /budgets/:budgetId', () => {
+    it('should update an existing budget', async () => {
+      const updateBudgetDto: UpdateBudgetDto = {
+        name: 'Updated Budget',
+        currency: ECurrency.EUR,
+      };
+
+      const response = await request(app.getHttpServer())
+        .put(`/budgets/${budgetId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updateBudgetDto)
+        .expect(HttpStatus.OK);
+
+      const budget = response.body;
+
+      expect(budget.name).toBe(updateBudgetDto.name);
+      expect(budget.currency).toBe(updateBudgetDto.currency);
+    });
+
+    it('should throw error because of invalid currency value', async () => {
+      const updateBudgetDto = {
+        name: 'Update Budget to invalid currency',
+        currency: 'invalid currency',
+        userId: userId,
+      };
+
+      await request(app.getHttpServer())
+        .put(`/budgets/${budgetId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updateBudgetDto)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('DELETE /budgets/:budgetId', () => {
+    it('should delete budget', async () => {
+      await request(app.getHttpServer())
+        .delete(`/budgets/${budgetId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.NO_CONTENT);
+    });
+
+    it('should throw error because it cant find budget with id', async () => {
+      await request(app.getHttpServer())
+        .delete(`/budgets/${budgetId + 1}`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('/users/me (DELETE)', () => {
+    it('Delete own user', async () => {
+      await request(app.getHttpServer())
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.NO_CONTENT);
+    });
   });
 });
